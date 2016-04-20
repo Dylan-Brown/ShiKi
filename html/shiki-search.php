036029264c6dd52c1fad3d7eccfe29ca6815eec2
Dylan Brown <dylanbrown.illustration@gmail.com>
2016-04-19 20:04:41 -0400

    pass  search tags to php

--- html/shiki-search.php
+++ html/shiki-search.php
@@ -16,71 +16,81 @@ function dbConnect() {
 // our database
 $db = dbConnect();
 
-
 // get the javascript variables
 $q = $_REQUEST["q"];
+echo '<div>$q<div>';
 
 if ($q != "") {
 	$nums = str_split($q);
 	
 	$vals = [];
 	$names = [];
-	
-	if ($nums[0] == 1) {
+	echo 'starting the nums<div>';
+	if ((int) $nums[0] == 1) {
 		array_push($names, 'summer');
 	}
-	if ($nums[1] == 1) {
+	if ((int) $nums[1] == 1) {
 		array_push($names, 'winter');
 	} 
-	if ($nums[2] == 1) {
+	if ((int) $nums[2] == 1) {
 		array_push($names, 'fall');
 	} 
-	if ($nums[3] == 1) {
+	if ((int) $nums[3] == 1) {
 		array_push($names, 'spring');
 	} 
-	if ($nums[4] == 1) {
+	if ((int) $nums[4] == 1) {
 		array_push($names, 'belowten');
 	} 	
-	if ($nums[5] == 1) {
+	if ((int) $nums[5] == 1) {
 		array_push($names, 'tentothirty');
 	} 
-	if ($nums[6] == 1) {
+	if ((int) $nums[6] == 1) {
 		array_push($names, 'thirtytofifty');
 	} 
-	if ($nums[7] == 1) {
+	if ((int) $nums[7] == 1) {
 		array_push($names, 'fiftytoseventy');
 	} 
-	if ($nums[8] == 1) {
+	if ((int) $nums[8] == 1) {
 		array_push($names, 'seventytoninety');
 	} 
-	if ($nums[9] == 1) {
+	if ((int) $nums[9] == 1) {
 		array_push($names, 'aboveninety');
 	} 
-	if ($nums[10] == 1) {
+	if ((int) $nums[10] == 1) {
 		array_push($names, 'casual');
 	} 
-	if ($nums[11] == 1) {
+	if ((int) $nums[11] == 1) {
 		array_push($names, 'business');
 	} 
-	if ($nums[12] == 1) {
+	if ((int) $nums[12] == 1) {
 		array_push($names, 'party');
 	} 
+	
+	foreach ($names as $name) {
+		echo 'name=' . $name . '<div>';
+	}
 	
 	// create where clause of sql
 	$where = "";
 	$c = count($names);
-	for ($i = 1; $i <= $c - 1; $i++) {
-		$where = $where . "'$names[$i]'=1, " 
+	for ($i = 0; $i <= $c - 2; $i++) {
+		$where = $where . "$names[$i]=1 AND ";
 	}
 	$d = $c-1;
-	$where = $where . "'$names[$d]'=1";
+	$where = $where . "$names[$d]=1";
+	
+	echo $where . '<div>';
 	
 	// query the database
 	$sql = <<<SQL
 		SELECT *
-		FROM `recommendations`
+		FROM recommendations
 		WHERE $where
 SQL;
+	
+	echo $sql . '<div>';
+
+	$result = $db->query($sql);
 	if (!$result) {
 		// no results or a query with the error
 		echo "<strong>There was an error running the query: $db->error<br></strong>";
@@ -89,7 +99,7 @@ SQL;
 		// there are results; put into the array (or string)
 		$urls = [];
 		$urls_s = "";
-		foreach ($_result as $item_s) {
+		foreach ($result as $item_s) {
 			foreach ($item_s as $item) {
 				$urls_s = $urls_s . $item['url'];
 				array_push($urls, $item['url']);
